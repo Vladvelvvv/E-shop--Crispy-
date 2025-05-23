cab02dc65fb665f1dc3eef6f7058176a6d3ff085
Vladvelvvv <vladvelvvv@gmail.com>
2025-05-23 10:35:47 +0200

    first example

---
+++ main/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+def index(request):
+    
+    context = {
+        'title': 'Crispy'
+    }
+    
+    return render(request, "main/index.html", context)
+
+def about(request):
+    return render(request, "main/about.html")
