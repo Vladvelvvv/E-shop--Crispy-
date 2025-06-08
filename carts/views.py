c1ecaf8771d8adb0f1b26b040a391f1d07730f94
Vladvelvvv <vladvelvvv@gmail.com>
2025-06-08 20:24:49 +0200

    add to cart ajax and jsonresponse

--- carts/views.py
+++ carts/views.py
@@ -1,7 +1,10 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import redirect, render
+from django.template.loader import render_to_string
 
 from carts.models import Cart
+from carts.templatetags.carts_tags import user_carts
+from carts.utils import get_user_carts
 from goods.models import Products
 
 def cart(request):
@@ -13,31 +16,43 @@ def cart(request):
     
     return render(request, "carts/cart.html", context=context)
 
-def cart_add(request, product_slug):
+def cart_add(request):
     
-    product = Products.objects.get(slug=product_slug)
+    product_id = request.POST.get("product_id")
+    product = Products.objects.get(id=product_id)
     
     if request.user.is_authenticated:
         carts = Cart.objects.filter(user=request.user, product=product)
 
-    if carts.exists():
-        cart = carts.first()
-        if cart:
-            cart.quantity += 1
-            cart.save()
-    else:
-        Cart.objects.create(user=request.user, product=product, quantity=1)
+        if carts.exists():
+            cart = carts.first()
+            if cart:
+                cart.quantity += 1
+                cart.save()
+        else:
+            Cart.objects.create(user=request.user, product=product, quantity=1)
 
-    return redirect(request.META['HTTP_REFERER'])
+    user_cart = get_user_carts(request)
 
-def cart_remove(request, cart_id):
+    cart_items_html = render_to_string(
+        "carts/includes/included_cart.html", {"carts": user_cart}, request=request
+    )
+    
+    response_data = {
+        "message": "Товар добавлен в корзину",
+        "cart_items_html": cart_items_html,
+    }
+    
+    return JsonResponse(response_data)
+
+def cart_remove(request):
          
     cart = Cart.objects.get(id=cart_id)
     cart.delete()
     
     return redirect(request.META['HTTP_REFERER'])
  
-def cart_change(request, product_slug):
+def cart_change(request):
     ...
 
 
