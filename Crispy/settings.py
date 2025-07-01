660aa55dbb4205269c0e36c6bada92e5732f06f8
Vladvelvvv <vladvelvvv@gmail.com>
2025-07-01 10:40:07 +0200

    for git

--- Crispy/settings.py
+++ Crispy/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-g1@fonra&zly63i$p_alp774jxy#h^06ik#5)9m+55f(x5@pf5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['crispy-mtc6.onrender.com']
 
 
 # Application definition
