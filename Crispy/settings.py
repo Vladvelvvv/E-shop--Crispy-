b23d1417a43d793fb42f1fa9a1ecb50c9074c3be
Vladvelvvv <126449916+Vladvelvvv@users.noreply.github.com>
2025-07-26 22:07:48 +0200

    Update settings.py

--- Crispy/settings.py
+++ Crispy/settings.py
@@ -61,8 +61,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
