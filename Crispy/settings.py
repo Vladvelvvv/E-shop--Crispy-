1fe677ff69b952ec4ee7dc257e79fee34bee492f
Vladvelvvv <126449916+Vladvelvvv@users.noreply.github.com>
2025-07-26 21:49:02 +0200

    Update settings.py

--- Crispy/settings.py
+++ Crispy/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-g1@fonra&zly63i$p_alp774jxy#h^06ik#5)9m+55f(x5@pf5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['crispy-cnhu.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
