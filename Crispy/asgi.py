f1cf975f3322b850f95a2f1ad4bbcf9fe92cc7cf
Vladvelvvv <126449916+Vladvelvvv@users.noreply.github.com>
2025-07-27 10:57:45 +0200

    Update asgi.py

--- Crispy/asgi.py
+++ Crispy/asgi.py
@@ -10,7 +10,8 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Crispy.settings')
 
-application = get_asgi_application()
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'media'), prefix='media/')
