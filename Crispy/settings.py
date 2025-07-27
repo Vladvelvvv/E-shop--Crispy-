61e9243dccac919912475ebf33a83bb5237c7336
Vladvelvvv <vladvelvvv@gmail.com>
2025-07-27 09:05:31 +0200

    Resolve merge conflict

--- Crispy/settings.py
+++ Crispy/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-g1@fonra&zly63i$p_alp774jxy#h^06ik#5)9m+55f(x5@pf5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['crispy-mtc6.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -59,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
