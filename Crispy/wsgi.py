af8dd9fcc8de7d1d123600819d3837a250f680c2
Vladvelvvv <126449916+Vladvelvvv@users.noreply.github.com>
2025-07-27 10:48:56 +0200

    Update wsgi.py

--- Crispy/wsgi.py
+++ Crispy/wsgi.py
@@ -10,7 +10,8 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Crispy.settings')
 
-application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'media'), prefix='media/')
